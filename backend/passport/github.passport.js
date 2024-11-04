a7ab4b0fbe478f41ab9595577f7a0f0c62690ac1
ujala <singhujala2004@gmail.com>
2024-11-04 20:25:10 +0530

    fix: update GitHub callback URL for local development

--- backend/passport/github.passport.js
+++ backend/passport/github.passport.js
@@ -15,7 +15,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "/api/auth/github/callback"
+    callbackURL: "http://localhost:5000/api/auth/github/callback"
 },
     async function (accessToken, refreshToken, profile, done) {
         const user = await userModel.findOne({ username: profile.username });
